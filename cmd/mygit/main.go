a1f5982c2d3ec573f71ce7eb3ec0618e597b4614
Pablo Fonseca <pablofonseca777@gmail.com>
2025-02-01 19:59:16 +0000

    second stage

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"path"
+	"strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
@@ -30,6 +35,46 @@ func main() {
 
 		fmt.Println("Initialized git directory")
 
+	case "cat-file":
+		if len(os.Args) < 4 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file <type> <object>\n")
+			os.Exit(1)
+		}
+		// flag := os.Args[2]
+		object := os.Args[3]
+
+		dirName := object[:2]
+		blobSha := object[2:]
+
+		buff, err := os.ReadFile(path.Join(".git", "objects", dirName, blobSha))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading file content: %s\n", err)
+			os.Exit(1)
+		}
+
+		b := bytes.NewReader(buff)
+		r, err := zlib.NewReader(b)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "zlib error: %s\n", err)
+			os.Exit(1)
+		}
+		defer r.Close()
+
+		var out bytes.Buffer
+		_, err = io.Copy(&out, r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading decompressed data: %s\n", err)
+			os.Exit(1)
+		}
+		data := out.String()
+		nullIdx := strings.IndexByte(data, '\x00')
+		if nullIdx == -1 {
+			fmt.Fprintf(os.Stderr, "invalid git object format\n")
+			os.Exit(1)
+		}
+
+		content := data[nullIdx+1:]
+		fmt.Print(content)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
